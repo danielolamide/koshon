7eda5515b38dfefa722d1196fd5a5f5c4e88a5a1
dannyolamide <dannyolamide@gmail.com>
2024-04-29 00:35:31 +0100

    build basic ui with streamlit

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,14 @@ from typing import List, Tuple
 import networkx as nx
 import matplotlib.pyplot as plt
 from urllib.parse import urlparse
+from termcolor import colored
 
 server_variables = {
     "QUERY_STRING": {"get": lambda x: urlparse(x).query},
 }
 
-backreferences = {}
 
-
-def parse_httpd_config(config_file_path):
+def parse_httpd_config(config):
     # Regular expressions for matching RewriteRule and RewriteCond directives
     rule_pattern = re.compile(r"RewriteRule\s+(.+?)\s+(.+?)(?:\s*\[(.*?)\])?\s*$")
     cond_pattern = re.compile(r"RewriteCond\s+(.+?)\s+(.+?)(?:\s*\[(.*?)\])?\s*$")
@@ -20,48 +19,49 @@ def parse_httpd_config(config_file_path):
     graph = nx.DiGraph()
     node_index = 0
 
-    with open(config_file_path, "r") as config_lines:
+    # with open(config, "r") as config_lines:
+    current_rule = None
+    current_conds = []  # List to store multiple conditions associated with a rule
+    for line in config.splitlines():
+        # for line in config_lines:
+        line = line.strip()
+        # Match RewriteConds
+        cond_match = cond_pattern.match(line)
+        if cond_match:
+            teststring, condition, flags = cond_match.groups()
+            graph.add_node(
+                node_index,
+                type="Cond",
+                teststring=teststring,
+                condition=condition,
+                flags=flags,
+                line=line,
+            )
+            current_conds.append(node_index)  # Append cond node index to the list
+            node_index += 1
 
-        current_rule = None
-        current_conds = []  # List to store multiple conditions associated with a rule
+        # Match RewriteRules
+        rule_match = rule_pattern.match(line)
+        if rule_match:
+            source_url, target_url, flags = rule_match.groups()
+            graph.add_node(
+                node_index,
+                type="Rule",
+                source=source_url,
+                target=target_url,
+                flags=flags,
+                line=line,
+            )
+            current_rule = node_index
+            node_index += 1
 
-        for line in config_lines:
-            line = line.strip()
-
-            # Match RewriteConds
-            cond_match = cond_pattern.match(line)
-            if cond_match:
-                teststring, condition, flags = cond_match.groups()
-                graph.add_node(
-                    node_index,
-                    type="Cond",
-                    teststring=teststring,
-                    condition=condition,
-                    flags=flags,
-                )
-                current_conds.append(node_index)  # Append cond node index to the list
-                node_index += 1
-
-            # Match RewriteRules
-            rule_match = rule_pattern.match(line)
-            if rule_match:
-                source_url, target_url, flags = rule_match.groups()
-                graph.add_node(
-                    node_index,
-                    type="Rule",
-                    source=source_url,
-                    target=target_url,
-                    flags=flags,
-                )
-                current_rule = node_index
-                node_index += 1
-
-                # Link each condition to the current rule
-                for cond_node in current_conds:
-                    graph.add_edge(cond_node, current_rule)
-                current_conds = []  # Reset the list for the next rule
+            # Link each condition to the current rule
+            for cond_node in current_conds:
+                graph.add_edge(cond_node, current_rule)
+            current_conds = []  # Reset the list for the next rule
 
     print("Nodes:", graph.nodes(data=True))
+    print("Edges:", graph.edges)
     print(graph)
 
     return graph
@@ -85,8 +85,10 @@ def simulate_request_flow(graph: nx.DiGraph, uri: str):
                 )
             )
             predecessors = list(graph.predecessors(node_index))
+            print("Predecessors:", predecessors)
+            print("Predecessors Type:", type(predecessors))
             if predecessors:
-                rule_to_conds.setdefault(node_index, []).append(*predecessors)
+                rule_to_conds.setdefault(node_index, []).extend(predecessors)
 
     for rule in rewrite_rules:
         conds = rule_to_conds.get(rule[0], [])
@@ -102,15 +104,15 @@ def simulate_request_flow(graph: nx.DiGraph, uri: str):
             )
         current_uri = rewrite_engine(rule, corresponding_conds, current_uri)
 
+    print(colored(f"Final URI to be served {current_uri}", "green"))
+    return current_uri
+
 
 def rewrite_engine(rule: List, conditions: List[Tuple], uri: str):
     request = get_uri_segments(uri)
     path = request.path
     rule_pattern = rule[1]
     target = rule[2]
-    print(
-        f"Evaluating RewriteRule {rule_pattern} against {path} with conditions {conditions}"
-    )
     rule_pattern_match = re.match(rule_pattern, path)
     url_changed = False
     cond_backreferences = {}
@@ -142,10 +144,11 @@ def rewrite_engine(rule: List, conditions: List[Tuple], uri: str):
         if rule_backreferences:
             for key, value in rule_backreferences.items():
                 path = path.replace("$" + key, value)
-        request = request._replace(path=path)
-        print(f"Url transfomed to {path}")
+        request = request._replace(path=path, query=None)
+        print(colored(f"URL Returned: {request.geturl()}", "green"))
         # request = request._replace(path=rule[2])
-    # else:
+    else:
+        print(colored(f"URL Returned: {request.geturl()}", "red"))
     request = request.geturl()
     return request
 
@@ -156,8 +159,8 @@ def get_uri_segments(request_uri: str):
 
 def validate_condition(teststring: str, condition: str, request_uri) -> Tuple:
     # Teststring value has to be determined - it is usually some sort of variable (apache variable or user variable)
+    backreferences = {}
     teststring = expand_variable(teststring)
-    print(f"Validating {teststring} meets condition {condition}")
     if teststring in server_variables:
         teststring = server_variables[teststring]["get"](request_uri)
     condition_matched = re.match(condition, teststring)
@@ -171,6 +174,13 @@ def validate_condition(teststring: str, condition: str, request_uri) -> Tuple:
 
 def expand_variable(var: str) -> str:
     var_pattern = re.compile(r"%{([^}]+)}")
-    print(f"Evaluating variable for expansion: {var}")
     var_found = var_pattern.match(var)
     return var_found.group(1) if var_found else var
+
+
+def visualize_graph(graph: nx.DiGraph):
+    fig = plt.figure()
+    labels = nx.get_node_attributes(graph, "line")
+    # node_positions = nx.multipartite_layout(graph)
+    nx.draw_networkx(graph, with_labels=True, labels=labels, font_size=5)
+    return fig
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from app import (
     parse_httpd_config,
     simulate_request_flow,
+    visualize_graph,
 )
+import streamlit as st
 
 if __name__ == "__main__":
-    conf_graph = parse_httpd_config("./app/httpd-rewrite.conf")
-    simulate_request_flow(
-        conf_graph, "https://example.com:3000/redir-to/xyz?name=daniel"
-    )
+    # conf_graph = parse_httpd_config("./app/httpd-rewrite.conf")
+    # simulate_request_flow(
+    #     conf_graph, "https://example.com:3000/redir-to/xyz?name=daniel"
+    # )
+    st.title("Koshon")
+    st.header("Test Apache Pre-deployment")
+    with st.form("apache_config_form"):
+        url = st.text_input(
+            label="Enter your request URL",
+            help="The rules in your configuration file will be executed against this URL",
+        )
+        httpd_config = st.text_area(label="Paste your Apache configuration here")
+        submit_config = st.form_submit_button(label="Submit")
 
+        if submit_config:
+            if all((httpd_config, url)):
+                graph = parse_httpd_config(httpd_config)
+                final_url = simulate_request_flow(graph, url)
+                fig = visualize_graph(graph)
+                st.text(f"Served URL: {final_url}")
+                st.pyplot(fig)
+            else:
+                print("not submitted")
