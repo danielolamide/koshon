ca86dfecefa24a2a544a1497451629f7686b2521
dannyolamide <dannyolamide@gmail.com>
2024-05-06 22:12:07 +0100

    enable simulation loop when URL is modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
+from enum import Enum
 import re
 from typing import List, Tuple
 import networkx as nx
 import matplotlib.pyplot as plt
 from urllib.parse import urlparse
-from termcolor import colored
+from termcolor import colored, cprint
 
 server_variables = {
     "QUERY_STRING": {"get": lambda x: urlparse(x).query},
+    "REQUEST_URI": {"get": lambda x: urlparse(x).path},
 }
+EngineAction = Enum("EngineAction", ["NO_CHANGE", "REWRITE"])
 
 
 def parse_httpd_config(config):
@@ -15,13 +18,14 @@ def parse_httpd_config(config):
     rule_pattern = re.compile(r"RewriteRule\s+(.+?)\s+(.+?)(?:\s*\[(.*?)\])?\s*$")
     cond_pattern = re.compile(r"RewriteCond\s+(.+?)\s+(.+?)(?:\s*\[(.*?)\])?\s*$")
 
-    # Initialize graph and node attribute index
+    # maintain node order of processing by initializing
     graph = nx.DiGraph()
     node_index = 0
 
     # with open(config, "r") as config_lines:
     current_rule = None
-    current_conds = []  # List to store multiple conditions associated with a rule
+    # Make it easier to store multiple corresponding conditions for a rule
+    current_conds = []
     for line in config.splitlines():
         # for line in config_lines:
         line = line.strip()
@@ -67,11 +71,10 @@ def parse_httpd_config(config):
     return graph
 
 
-def simulate_request_flow(graph: nx.DiGraph, uri: str):
-    print(f"Simulating request to {uri}")
+def simulate_request_flow(graph: nx.DiGraph, url: str):
     rewrite_rules = []
     rule_to_conds = {}
-    current_uri = uri
+    # the request has to pass through all nodes, still maintaining the order they were defined in the httpd.conf
     for node_index in graph.nodes():
         node_data = graph.nodes[node_index]
         node_type = node_data["type"]
@@ -85,55 +88,85 @@ def simulate_request_flow(graph: nx.DiGraph, uri: str):
                 )
             )
             predecessors = list(graph.predecessors(node_index))
-            print("Predecessors:", predecessors)
-            print("Predecessors Type:", type(predecessors))
             if predecessors:
                 rule_to_conds.setdefault(node_index, []).extend(predecessors)
 
-    for rule in rewrite_rules:
-        conds = rule_to_conds.get(rule[0], [])
-        corresponding_conds = []
-        for cond in conds:
-            corresponding_cond = graph.nodes[cond]
-            corresponding_conds.append(
-                (
-                    corresponding_cond["teststring"],
-                    corresponding_cond["condition"],
-                    corresponding_cond["flags"],
-                )
-            )
-        current_uri = rewrite_engine(rule, corresponding_conds, current_uri)
+    # the REQUEST_URI will be looped around the entire ruleset until no more rules match
+    # while action is not no_change
+    # take request_uri through rewrite_engine
+    # output from rewrite_engine should include action and current request url
+    # we would then return the request url returned with either serve/ext_redirect
+    engine_action = EngineAction.REWRITE
+    current_url = url
+    engine_run_count = 0
 
-    print(colored(f"Final URI to be served {current_uri}", "green"))
-    return current_uri
+    while engine_action != EngineAction.NO_CHANGE:
+        cprint(f"Simulation #{engine_run_count}", "white", "on_light_cyan")
+        print(f"Simulating request to {current_url}")
+        urls = []
+        actions = []
+        # eval whether url is internal/external URL
+        for rule in rewrite_rules:
+            conds = rule_to_conds.get(rule[0], [])
+            corresponding_conds = []
+            for cond in conds:
+                corresponding_cond = graph.nodes[cond]
+                corresponding_conds.append(
+                    (
+                        corresponding_cond["teststring"],
+                        corresponding_cond["condition"],
+                        corresponding_cond["flags"],
+                    )
+                )
+            current_url, action = rewrite_engine(rule, corresponding_conds, current_url)
+            actions.append(action)
+            urls.append(current_url)
+
+        if EngineAction.REWRITE not in actions:
+            engine_action = EngineAction.NO_CHANGE
+
+        engine_run_count += 1
+
+    cprint(f"Final URI to be served {current_url}", "green")
+    return current_url
 
 
 def rewrite_engine(rule: List, conditions: List[Tuple], uri: str):
-    request = get_uri_segments(uri)
+    cprint(f"Rule: {rule}, conditions: {conditions}", "white", "on_dark_grey")
+    request = urlparse(uri)
     path = request.path
-    rule_pattern = rule[1]
+    rule_pattern = rf"{rule[1]}"
     target = rule[2]
+    print(rule_pattern, path)
     rule_pattern_match = re.match(rule_pattern, path)
-    url_changed = False
+    valid_conditions = False
     cond_backreferences = {}
     rule_backreferences = {}
     if rule_pattern_match:
+        cprint("Pattern matched", "black", "on_green")
         # create rule backreferences
         for i, val in enumerate(rule_pattern_match.groups(), start=1):
             rule_backreferences[str(i)] = val
         # Corresponding rule conditions must be evaluated before transformation
+        # unless [OR] flag exists, we expect all conditions to be true for rule to be applied
         for condition in conditions:
             teststring = condition[0]
-            pattern = condition[1]
-            url_changed, cond_backreferences = validate_condition(
+            pattern = rf"{condition[1]}"
+            cond_flags = condition[2]
+            is_cond_valid, cond_backreferences = validate_condition(
                 teststring, pattern, uri
             )
-            if url_changed:
+            if is_cond_valid:
+                valid_conditions = True
                 continue
             else:
+                valid_conditions = False
                 break
+    else:
+        cprint("No match", "black", "on_light_yellow")
+        return request.geturl(), EngineAction.NO_CHANGE
 
-    if url_changed:
+    if valid_conditions:
         # Distinguish between external and internal redirect
         path = request.path
         print(f"Rewriting {rule_pattern} to {target}")
@@ -145,37 +178,40 @@ def rewrite_engine(rule: List, conditions: List[Tuple], uri: str):
             for key, value in rule_backreferences.items():
                 path = path.replace("$" + key, value)
         request = request._replace(path=path, query=None)
-        print(colored(f"URL Returned: {request.geturl()}", "green"))
-        # request = request._replace(path=rule[2])
+        print(colored(f"URL Rewrite: {request.geturl()}", "green"))
+        return request.geturl(), EngineAction.REWRITE
     else:
-        print(colored(f"URL Returned: {request.geturl()}", "red"))
-    request = request.geturl()
-    return request
-
-
-def get_uri_segments(request_uri: str):
-    return urlparse(request_uri)
+        print(colored(f"No valid corresponding conditions", "red"))
+        return request.geturl(), EngineAction.NO_CHANGE
 
 
 def validate_condition(teststring: str, condition: str, request_uri) -> Tuple:
-    # Teststring value has to be determined - it is usually some sort of variable (apache variable or user variable)
+    # Teststring value has to be determined - usually a variable (apache variable or user variable)
     backreferences = {}
     teststring = expand_variable(teststring)
+    cprint(f"Validating conditions {teststring}", "black", "on_yellow")
     if teststring in server_variables:
         teststring = server_variables[teststring]["get"](request_uri)
+    else:
+        cprint(f"{teststring} not found in variables")
     condition_matched = re.match(condition, teststring)
     if condition_matched:
         for i, val in enumerate(condition_matched.groups(), start=1):
             backreferences[str(i)] = val
+        cprint(f"Validation passed {teststring}", "black", "on_green")
         return True, backreferences
     else:
+        cprint(f"Validation failed {teststring} {condition}", "red", "on_light_red")
         return False, None
 
 
 def expand_variable(var: str) -> str:
+    """
+    Get httpd variable and value
+    """
     var_pattern = re.compile(r"%{([^}]+)}")
-    var_found = var_pattern.match(var)
-    return var_found.group(1) if var_found else var
+    httpd_var = var_pattern.match(var)
+    return httpd_var.group(1) if httpd_var else ""
 
 
 def visualize_graph(graph: nx.DiGraph):
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from app import (
     visualize_graph,
 )
 import streamlit as st
+import os
 
 if __name__ == "__main__":
     # conf_graph = parse_httpd_config("./app/httpd-rewrite.conf")
     # simulate_request_flow(
     #     conf_graph, "https://example.com:3000/redir-to/xyz?name=daniel"
     # )
+    os.system("clear")
     st.title("Koshon")
     st.header("Test Apache Pre-deployment")
     with st.form("apache_config_form"):
